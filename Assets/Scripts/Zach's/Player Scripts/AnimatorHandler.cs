a16b4d9592d596fb9e684de76f1ce739a04d692d
Zachary Karnes <zach.karnes@outlook.com>
2021-04-21 14:52:27 -0600

    Player stuff
    working on some stuff to make the swords look good

---
+++ Assets/Scripts/Zach's/Player Scripts/AnimatorHandler.cs
@@ -0,0 +1,102 @@
+﻿using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+public class AnimatorHandler : MonoBehaviour
+
+{
+
+    private Animator anim;
+
+
+
+    public bool canRotate = true;
+    public bool daggerDrawn = false;
+    public bool swordDrawn = false;
+
+
+
+    public void Initialize()
+
+    {
+
+        anim = GetComponentInChildren<Animator>();
+
+    }
+
+
+
+    public void UpdateAnimatorValues(float vMov, float hMov)
+
+    {
+
+        anim.SetFloat("vertical", vMov, .1f, Time.deltaTime);
+
+
+
+    }
+
+
+
+    public void StopRotation()
+
+    {
+
+        canRotate = false;
+
+    }
+
+
+
+    public void StartRotation()
+
+    {
+
+        canRotate = true;
+
+    }
+
+
+
+    public void UpdateJump(bool jumping)
+
+    {
+
+        anim.SetBool("Jump", jumping);
+      
+
+    }
+    public void UpdateRAttack(bool RAttacking) {
+
+       
+
+        anim.SetBool("RightA", RAttacking);
+        
+
+    }
+   
+    public void UpdateLAttack(bool LAttacking)
+    {
+        
+        anim.SetBool("LeftA", LAttacking);
+       
+    }
+
+
+    public void UpdateDodge(bool dodging) {
+        anim.SetBool("Dodge", dodging);
+
+    }
+
+    public void UpdateAttack(bool attacking) {
+        anim.SetBool("Attacking", attacking);
+    }
+
+    public void UpdateDeath(bool death) {
+        anim.SetBool("Dead", death);
+    }
+}
---
+++ Assets/Scripts/Zach's/Player Scripts/PlayerController.cs
@@ -0,0 +1,278 @@
+﻿using System;
+using System.Collections;
+
+using System.Collections.Generic;
+
+using UnityEngine;
+
+
+
+public class PlayerController : MonoBehaviour
+
+{
+
+    //setup vars
+
+    private InputHandler input;
+
+    private CharacterController controller;
+
+    private AnimatorHandler animatorHandler;
+
+    
+  
+
+
+
+    public Transform camParent;
+
+    public Transform cam;
+
+
+
+    //control stats
+
+    public float speed = 8f;
+
+    public float rotationSpeed = 10f;
+
+
+
+    //gravity and jumping
+
+    public bool grounded = true;
+
+    public float gravity = -9.81f;
+
+    public float jumpHeight = 2;
+
+    private float groundDist = .2f;
+
+
+
+    public Transform groundCheck;
+
+    public LayerMask groundMask;
+
+
+
+    private Vector3 velocity;
+
+    
+
+    
+
+
+    void Start()
+
+    {
+
+        input = InputHandler.instance;
+
+        controller = GetComponent<CharacterController>();
+
+        animatorHandler = GetComponent<AnimatorHandler>();
+    
+        animatorHandler.Initialize();
+
+    }
+
+
+
+    void Update()
+
+    {
+
+        HandleMovement(Time.deltaTime);
+
+        HandleRotation(Time.deltaTime);
+
+        HandleGravity(Time.deltaTime);
+
+        HandleJump();
+
+        HandleAttack();
+        HandleDodge();
+        
+
+
+
+        animatorHandler.UpdateAnimatorValues(input.moveAmount, 0);
+
+    }
+
+
+
+    private void HandleMovement(float delta)
+
+    {
+
+        Vector3 movement = (input.move.x * camParent.right) + (input.move.y * camParent.forward);
+
+        controller.Move(movement * speed * delta);
+
+    }
+
+    private void HandleRotation(float delta)
+
+    {
+
+        Vector3 targetDir;
+
+
+
+        targetDir = cam.forward * input.move.y;
+
+
+
+        targetDir += cam.right * input.move.x;
+
+
+
+        targetDir.Normalize();
+
+
+
+        targetDir.y = 0;
+
+
+
+        if (targetDir == Vector3.zero)
+
+        {
+
+            targetDir = transform.forward;
+
+        }
+
+
+
+        Quaternion tr = Quaternion.LookRotation(targetDir);
+
+
+
+        transform.rotation = Quaternion.Slerp(transform.rotation, tr, rotationSpeed * delta);
+
+    }
+
+
+
+    private void HandleGravity(float delta)
+
+    {
+
+        grounded = Physics.CheckSphere(groundCheck.position, groundDist, groundMask);
+
+
+
+        if (grounded && velocity.y < 0)
+
+        {
+
+            velocity.y = -2f;
+
+        }
+
+
+
+        velocity.y += gravity * delta;
+
+        controller.Move(velocity * delta);
+
+
+
+    }
+
+
+
+    private void HandleJump()
+    {
+
+
+
+        if (input.jumpDown && grounded)
+
+        {
+
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+
+            animatorHandler.UpdateJump(true);
+
+        }
+
+        else if (grounded)
+        {
+
+            animatorHandler.UpdateJump(false);
+
+        }
+
+
+
+    }
+
+
+    public void HandleAttack()
+    {
+
+        if (input.rAttackOn)
+        {
+            HandleRightAttack();
+        } else
+        {
+            animatorHandler.UpdateRAttack(false);
+        }
+
+        if (input.lAttackOn)
+        {
+            HandleLeftAttack();
+        } else
+        {
+            animatorHandler.UpdateLAttack(false);
+        }
+ 
+    }
+
+    private void HandleRightAttack()
+    {
+        if (!animatorHandler.swordDrawn)
+        {
+          
+        } else
+        {
+            animatorHandler.UpdateAttack(true);
+            animatorHandler.UpdateRAttack(true);
+        }
+    }
+
+    private void HandleLeftAttack()
+    {
+        if (!animatorHandler.daggerDrawn)
+        {
+           
+        } else
+        {
+            animatorHandler.UpdateLAttack(true);
+            animatorHandler.UpdateAttack(true);
+        }
+    }
+
+    private void HandleDodge() {
+        if (input.dodgeOn)
+        {
+            animatorHandler.UpdateDodge(true);
+        }
+        else if (!input.dodgeOn) {
+            animatorHandler.UpdateDodge(false);
+        }
+    }
+
+    //private void HandleDeath() {
+    //    if (!battle.isPlayerAlive) {
+     //       animatorHandler.UpdateDeath(true);
+       // } else if (battle.isPlayerAlive) {
+         //   animatorHandler.UpdateDeath(false);
+       // }
+    //}
+}
+
