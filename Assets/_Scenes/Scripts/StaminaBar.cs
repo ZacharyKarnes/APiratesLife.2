116efce1539070a555418441dcbfdcd878130813
Auston <Ao11939@ms.wasatch.edu>
2021-04-27 13:42:59 -0600

    Stamina and Fixed Main Menu
    Stamina has been finished and fixed main menu

---
+++ Assets/_Scenes/Scripts/StaminaBar.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class StaminaBar : MonoBehaviour
+{
+    public Slider staminaBar;
+
+    private int maxStamina = 100;
+    private int currentStamina;
+
+    public static StaminaBar instance;
+
+    private void Awake()
+    {
+        instance = this;
+    }
+
+    void Start()
+    {
+        currentStamina = maxStamina;
+        staminaBar.maxValue = maxStamina;
+        staminaBar.value = maxStamina;
+    }
+
+    public void UseStamina(int amount)
+    {
+        if(currentStamina - amount >= 0)
+        {
+            currentStamina -= amount;
+            staminaBar.value = currentStamina;
+        }
+        else
+        {
+            Debug.Log("Workin");
+        }
+    }
+}
