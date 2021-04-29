b0e33a7aa3df65b060ecee9deb218c35aec8b434
Zachary Karnes <zach.karnes@outlook.com>
2021-04-29 14:29:34 -0600

    Basic changes
    more animations

--- Assets/Scripts/Zach's/Player Scripts/PlayerBattleStats.cs
+++ Assets/Scripts/Zach's/Player Scripts/PlayerBattleStats.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class PlayerBattleStats : MonoBehaviour
 {
+
+    private AnimatorHandler animatorHandler;
+
+
     //player variables
     public int health;
     public int stamina;
-   
-    
+
+
     //damage variables
     public bool damageTaken;
     public int amountOfDamageTaken;
@@ -17,30 +21,13 @@ public class PlayerBattleStats : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        animatorHandler = GetComponentInChildren<AnimatorHandler>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        PlayerHealth();
-    }
-
-    private void PlayerHealth() {
-        health = 100;
-        if (damageTaken)
-        {
-            TakeDamage();
-        }
-        else if (!damageTaken) {
-            
-        }
-    }
-    private void DealDamage() {
 
     }
-    private void TakeDamage()
-    {
-        amountOfDamageTaken = 1;
-    }
-    }
+}
+
